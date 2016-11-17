fb8b0a0ea9c2713c3707a7dcd4d9bd0623a1ef34
Matthew Vandenbossche <matt@icodethin.gs>
2016-11-17 14:58:10 +1100

    Rewrite!

---
+++ index.js
@@ -0,0 +1,5 @@
+exports.Timed = require('./lib/TimedBudget');
+exports.Fixed = require('./lib/FixedBudget');
+exports.Ensure = require('./lib/EnsureBudget');
+exports.Split = require('./lib/SplitBudget');
+exports.Budget = require('./lib/Budget');
